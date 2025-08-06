d2b758141341f1f7839aa109780a53cc42197932
JE Pacres <pacresjohn029@gmail.com>
2025-08-06 23:14:33 +0800

    [Feature] blogs, navbar, footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>Nav Bar</nav>
         {children}
+        <footer>Footer</footer>
       </body>
     </html>
   );
